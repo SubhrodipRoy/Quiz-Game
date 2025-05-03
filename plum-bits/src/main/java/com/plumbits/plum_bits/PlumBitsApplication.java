a25db3621bd76f4d89aeeedf8615fe2c9f0166b9
Subhrodip Roy <roysubhrodiproy@gmail.com>
2025-05-03 15:59:31 +0530

    doing frontend

---
+++ plum-bits/src/main/java/com/plumbits/plum_bits/PlumBitsApplication.java
@@ -0,0 +1,13 @@
+package com.plumbits.plum_bits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumBitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumBitsApplication.class, args);
+	}
+
+}
---
+++ plum-bits/src/test/java/com/plumbits/plum_bits/PlumBitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plumbits.plum_bits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumBitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
